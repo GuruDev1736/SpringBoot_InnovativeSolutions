7680c7e8b5165fa01733cdfbfcf0b6c5e751d291
Guruprasad <guruprasad1736@gmail.com>
2023-06-29 17:42:09 +0530

    Initial commit

---
+++ src/main/java/com/InnovativeSolutions/project/InnovativeSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.InnovativeSolutions.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovativeSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovativeSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InnovativeSolutions/project/InnovativeSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InnovativeSolutions.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovativeSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
